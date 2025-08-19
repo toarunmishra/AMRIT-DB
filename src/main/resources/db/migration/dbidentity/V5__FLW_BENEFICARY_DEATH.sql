d040ed41ca5d4b5cc98981f5ed3776f68c249ac1
Saurav Mishra <saurav.mishra@bizbrolly.com>
2025-08-19 13:51:15 +0530

    beneficiarydetails_rmnch migration

--- src/main/resources/db/migration/dbidentity/V5__FLW_BENEFICARY_DEATH.sql
+++ src/main/resources/db/migration/dbidentity/V5__FLW_BENEFICARY_DEATH.sql
@@ -4,7 +4,7 @@ ALTER TABLE i_beneficiarydetails_rmnch
 ADD COLUMN `is_death` TINYINT(1) DEFAULT NULL,
 ADD COLUMN `is_death_value` TINYINT(1) DEFAULT NULL,
 ADD COLUMN `date_of_death` VARCHAR(200) DEFAULT NULL,
-ADD COLUMN `time_of_Death` VARCHAR(200) DEFAULT NULL,
+ADD COLUMN `time_of_death` VARCHAR(200) DEFAULT NULL,
 ADD COLUMN `reason_of_death_id` INT(11) DEFAULT 0,
 ADD COLUMN `place_of_death` VARCHAR(200) DEFAULT NULL,
 ADD COLUMN `place_of_death_id` INT(11) DEFAULT 0,
